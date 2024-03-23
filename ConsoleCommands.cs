8ddb75a17fc38ea799cd95720a5c6cdb21e3d532
MrJachowski21 <164620374+MrJachowski21@users.noreply.github.com>
2024-03-23 10:18:17 +0100

    ConsoleCommands.cs

--- ConsoleCommands.cs
+++ ConsoleCommands.cs
@@ -1,8 +1,211 @@
-﻿using System;
+using Cosmos.Core_Asm;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
-public class Class1
+namespace JachowskiOS.System
 {
-	public Class1()
-	{
-	}
+    public static class ConsoleCommands
+    {
+        public static void RunCommand(string command)
+        {
+            string[] words = command.Split(' ');
+            if (words.Length > 0)
+            {
+                if (words[0] == "info")
+                {
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine(WriteMessage.CenterText("JachowskiOS"));
+                    Console.WriteLine(WriteMessage.CenterText(Kernel.Version));
+                    Console.WriteLine(WriteMessage.CenterText("Created by Jachowski System "));
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else if (words[0] == "help")
+                {
+                    Console.WriteLine("Available commands:");
+                    Console.WriteLine("- info: Display system information");
+                    Console.WriteLine("- format: Format the disk");
+                    Console.WriteLine("- space: Check available free space");
+                    Console.WriteLine("- dir: List directories and files in the current directory");
+                    Console.WriteLine("- echo <text> > <file>: Write text to a file");
+                    Console.WriteLine("- cat <file>: Display the contents of a file");
+                    Console.WriteLine("- del <file>: Delete a file");
+                    Console.WriteLine("- mkdir <directory>: Create a new directory");
+                }
+
+                else if (words[0] == "format")
+                {
+                    if (Kernel.fs.Disks[0].Partitions.Count > 0)
+                    {
+                        Kernel.fs.Disks[0].DeletePartition(0);
+                    }
+                    Kernel.fs.Disks[0].Clear();
+                    Kernel.fs.Disks[0].CreatePartition(Kernel.fs.Disks[0].Size / (1024 * 1024));
+                    Kernel.fs.Disks[0].FormatPartition(0, "FAT32", true);
+                    WriteMessage.WriteOK("Succes!");
+                    WriteMessage.WriteWarn("JachowskiOS will reboot in 3 seconds!");
+                    Thread.Sleep(3000);
+                    Cosmos.System.Power.Reboot();
+                }
+                else if (words[0] == "space")
+                {
+                    long free = Kernel.fs.GetAvailableFreeSpace(Kernel.Path);
+                    Console.Write("Free space: " + free / 1024 + "kb");
+                }
+                else if (words[0] == "dir")
+                {
+                    var Directories = Directory.GetDirectories(Kernel.Path);
+                    var Files = Directory.GetFiles(Kernel.Path);
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Directories (" + Directories.Length + ")");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    for (int i = 0; i < Directories.Length; i++)
+                    {
+                        Console.WriteLine(Directories[i]);
+                    }
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Files (" + Files.Length + ")");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    for (int i = 0; i < Files.Length; i++)
+                    {
+                        Console.WriteLine(Files[i]);
+                    }
+                }
+                else if (words[0] == "echo")
+                {
+                    if (words.Length > 1)
+                    {
+                        string wholeString = "";
+                        for (int i = 1; i < words.Length; i++)
+                        {
+                            wholeString += words[i] + " ";
+                        }
+                        int pathIndex = wholeString.LastIndexOf('>');
+                        String text = wholeString.Substring(0, pathIndex);
+                        String path = wholeString.Substring(pathIndex + 1);
+                        if (!path.Contains(@"\"))
+                            path = Kernel.Path + path;
+                        if (path.EndsWith(' '))
+                        {
+                            path = path.Substring(0, path.Length - 1);
+                        }
+                        var file_stream = File.Create(path);
+                        file_stream.Close();
+                        File.WriteAllText(path, text);
+                    }
+                    else
+
+                        WriteMessage.WriteError("Invalid Syntax!");
+                }
+                else if (words[0] == "cat")//Wypisujemy zawartość pliku
+                {
+                    if (words.Length > 1)
+                    {
+                        string path = words[1];
+                        if (!path.Contains(@"\"))
+                            path = Kernel.Path + path;
+                        if (path.EndsWith(' '))
+                        {
+                            path = path.Substring(0, path.Length - 1);
+                        }
+                        if (File.Exists(path))
+                        {
+                            string text = File.ReadAllText(path);
+                            Console.ForegroundColor = ConsoleColor.Gray;
+                            Console.WriteLine(text);
+                        }
+                        else
+                            WriteMessage.WriteError("File " + path + " not found!");
+                    }
+                    else
+                        WriteMessage.WriteError("Invalid Syntax!");
+                }
+                else if (words[0] == "del")//Usuwamy plik
+                {
+                    if (words.Length > 1)
+                    {
+                        string path = words[1];
+                        if (!path.Contains(@"\"))
+                            path = Kernel.Path + path;
+                        if (path.EndsWith(' '))
+                        {
+                            path = path.Substring(0, path.Length - 1);
+                        }
+                        if (File.Exists(path))
+                        {
+                            File.Delete(path);
+                            WriteMessage.WriteOK("Deleted " + path + "!");
+                        }
+                        else
+                            WriteMessage.WriteError("File " + path + " not found!");
+                    }
+                    else
+                        WriteMessage.WriteError("Invalid Syntax!");
+                }
+                else if (words[0] == "mkdir")//Tworzymy folder
+                {
+                    if (words.Length > 1)
+                    {
+                        string path = words[1];
+                        if (!path.Contains(@"\"))
+                            path = Kernel.Path + path;
+                        if (path.EndsWith(' '))
+                        {
+                            path = path.Substring(0, path.Length - 1);
+                        }
+                        Directory.CreateDirectory(path);
+                    }
+                    else
+                        WriteMessage.WriteError("Invalid Syntax!");
+                }
+                else if (words[0] == "cd")//Zmieniamy Kernel.Path
+                {
+                    if (words.Length > 1)
+                    {
+                        if (words[1] == "..")
+                        {
+                            if (Kernel.Path != @"0:\")
+                            {
+                                string tempPath = Kernel.Path.Substring(0, Kernel.Path.Length - 1);
+                                Kernel.Path = tempPath.Substring(0, tempPath.LastIndexOf(@"\") + 1);
+                                return;
+                            }
+                            else
+                                return;
+                        }
+                        string path = words[1];
+                        if (!path.Contains(@"\"))
+                            path = Kernel.Path + path + @"\";
+                        if (path.EndsWith(' '))
+                        {
+                            path = path.Substring(0, path.Length - 1);
+                        }
+                        if (!path.EndsWith(@"\"))
+                            path += @"\";
+                        if (Directory.Exists(path))
+                            Kernel.Path = path;
+                        else
+                            WriteMessage.WriteError("Directory " + path + " not found!");
+                    }
+                    else
+                        Kernel.Path = @"0:\";
+
+
+
+                }
+                else
+                {
+                    WriteMessage.WriteError("Please enter a vaild command!");
+                }
+
+            }
+        }
+    }
 }
+
+
