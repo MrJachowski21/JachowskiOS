e109d922eb8ace470d6815d5b5aebf7b11ff8a92
MrJachowski21 <164620374+MrJachowski21@users.noreply.github.com>
2024-03-23 10:15:24 +0100

    Add files via upload

---
+++ Kernel.cs
@@ -0,0 +1,34 @@
+﻿using Cosmos.System.FileSystem;
+using JachowskiOS.System;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+
+namespace JachowskiOS
+{
+    public class Kernel : Sys.Kernel
+    {
+        public static string Version = "1.0.0";
+        public static string Path = @"0:\";
+        public static CosmosVFS fs;
+        protected override void BeforeRun()
+        {
+            Console.SetWindowSize(90, 30);
+            Console.OutputEncoding = Cosmos.System.ExtendedASCII.CosmosEncodingProvider.Instance.GetEncoding(437);
+            fs = new Cosmos.System.FileSystem.CosmosVFS();
+            Cosmos.System.FileSystem.VFS.VFSManager.RegisterVFS(fs);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Booting JachowskiOS " + Version);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        protected override void Run()
+        {
+            Console.Write(Path + ">");
+            var command = Console.ReadLine();
+            ConsoleCommands.RunCommand(command);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
